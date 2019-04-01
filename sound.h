d7312bad361b0c63afc072a9ac1870f0d4a0c8a6
Hai Vu <andreehai1011@gmail.com>
2019-04-01 09:43:13 +0300

    Fix indent sound.h

--- sound.h
+++ sound.h
@@ -8,16 +8,16 @@ struct WAVHDR{
 	char Format[4];		// it has to be "WAVE"
 
 	char Subchunk1ID[4];	// "fmt"
-	int Subchunk1Size;		// PCM = 16
-	short AudioFormat;		// should be 1
-	short NumChannels;		// should be 1 for mono
-	int SampleRate;			// 16000
-	int ByteRate;			// 16000*NumChannels*BitsPerSample/8
-	short BlockAlign;		// NumChannels*BitsPerSample/8
+	int Subchunk1Size;	// PCM = 16
+	short AudioFormat;	// should be 1
+	short NumChannels;	// should be 1 for mono
+	int SampleRate;		// 16000
+	int ByteRate;		// 16000*NumChannels*BitsPerSample/8
+	short BlockAlign;	// NumChannels*BitsPerSample/8
 	short BitsPerSample;	// in our app, 16 (-f S16_LE)
 	
 	char Subchunk2ID[4]; 	// data
-	int Subchunk1Size;		//
+	int Subchunk1Size;	//
 };
 
 //function declarations
