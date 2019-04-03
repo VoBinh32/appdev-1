ff435e6ff1b5b8c1d32b5370a752e9c489d4e433
Hai Vu <andreehai1011@gmail.com>
2019-04-03 16:17:10 +0300

    them dam ba cai tinh nang

--- main.c
+++ main.c
@@ -1,10 +1,34 @@
-// this is main.c
+//this is main.c
 #include <stdio.h>
-
-//func declaration
-void foo(void);
+#include <signal.h>
+#include <stdlib.h>
+#include <time.h>
+#include "screen.h"
+#include "sound.h"
 
 int main(){
-	foo();
-	printf("Printed from main.\n");
+	FILE *f;
+	short sd[RATE]; // for all sample in 1 sec
+	while(1){
+		int ret = system(CMD);
+		if(ret == SIGINT) break; // break signal by keyboard
+		
+		f = fopen("test.wav", "r");
+		clearScreen();
+		setColors(CYAN, bg(BLACK));
+
+		if(f == NULL){
+			printf("cannot open the wav file.\n");
+			return 1;
+		}
+		
+		struct WAVHDR h;	// instance of wave header
+		fread(&h, sizeof(h), 1, f);		// read wav header to h
+		fread(&sd, sizeof(sd), 1, f);	// read WAV data
+		fclose(f);
+		displayWAVHDR(h);	// show wav header information
+		displayWAVDATA(sd);	// show data wave data information		
+	}
+	
+	resetColors();
 }
--- sound.h
+++ sound.h
@@ -1,5 +1,6 @@
 // constants definition
 #define RATE 16000		// samples per second
+#define CMD "arecord -r16000 -c1 -f S16_LE -d1 -q test.wav"
 
 // data structures
 struct WAVHDR{
@@ -8,16 +9,18 @@ struct WAVHDR{
 	char Format[4];		// it has to be "WAVE"
 
 	char Subchunk1ID[4];	// "fmt"
-	int Subchunk1Size;	// PCM = 16
-	short AudioFormat;	// should be 1
-	short NumChannels;	// should be 1 for mono
-	int SampleRate;		// 16000
-	int ByteRate;		// 16000*NumChannels*BitsPerSample/8
-	short BlockAlign;	// NumChannels*BitsPerSample/8
+	int Subchunk1Size;		// PCM = 16
+	short AudioFormat;		// should be 1
+	short NumChannels;		// should be 1 for mono
+	int SampleRate;			// 16000
+	int ByteRate;			// 16000*NumChannels*BitsPerSample/8
+	short BlockAlign;		// NumChannels*BitsPerSample/8
 	short BitsPerSample;	// in our app, 16 (-f S16_LE)
 	
 	char Subchunk2ID[4]; 	// data
-	int Subchunk1Size;	//
+	int Subchunk2Size;	//
 };
 
 //function declarations
+void displayWAVHDR(struct WAVHDR h);
+void displayWAVDATA(short []);
